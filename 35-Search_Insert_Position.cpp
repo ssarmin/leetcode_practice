1853dfbeeab6645da15e9ba8de28810044ecb849
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2024-09-10 20:44:18 -0400

    Create 35-Search_Insert_Position.cpp

---
+++ 35-Search_Insert_Position.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/search-insert-position/description/
+class Solution {
+public:
+    void search(vector<int> nums, int start, int end, int target, int &index){
+        if(start > end)
+            return;
+        int mid = (end + start)/2;
+        if(nums[mid] == target){
+            index = mid;
+            return;
+        }else if(nums[mid] > target){
+            index = mid;
+            search(nums, start, mid-1, target, index);
+        }else{
+            index = mid+1;
+            search(nums, mid+1, end, target, index);
+        }
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        int index = -1;
+        search(nums, 0, nums.size()-1, target, index);
+        return index;
+    }
+};
+
+//Time complexity: O(nlogn)
