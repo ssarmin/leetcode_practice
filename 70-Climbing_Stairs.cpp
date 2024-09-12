43680362b912aa262c4c3b6e39175897a33f29b3
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2024-09-12 15:42:08 -0400

    Create 70-Climbing_Stairs.cpp

---
+++ 70-Climbing_Stairs.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n < 3)
+            return n;
+        vector<int> v(n+1, 0);
+        v[1] = 1;
+        v[2] = 2;
+        for(int i=3; i<=n; i++){
+            v[i] = v[i-1] + v[i-2];
+        }
+        return v[n];
+    }
+};
