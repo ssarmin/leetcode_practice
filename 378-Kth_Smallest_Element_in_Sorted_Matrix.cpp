034a28280177a7a466de371979b81b9881c7ba82
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2024-09-18 10:04:38 -0400

    Create 378-Kth_Smallest_Element_in_Sorted_Matrix.cpp

---
+++ 378-Kth_Smallest_Element_in_Sorted_Matrix.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+
+/*
+Extra testcases:
+[[3,6,9,12,17,22],[5,11,11,16,22,26],[10,11,14,16,24,31],[10,15,17,17,29,31],[14,17,20,23,34,37],[19,21,22,28,37,40]]
+35
+[[-5,-4],[-5,-4]]
+3
+[[1,2], [1,3]]
+2
+https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1323090/C%2B%2B-Solution
+*/
