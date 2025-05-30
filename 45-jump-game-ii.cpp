ff67845bd715cc3b3c59f4cf3ab909266d20a6d1
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-05-29 23:57:40 -0400

    Create 45-jump-game-ii.cpp

---
+++ 45-jump-game-ii.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/jump-game-ii
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> visit(nums.size(), INT_MAX);
+
+        visit[0] = 0;
+        for(int i=0; i<nums.size(); i++){
+            for(int k=1; k<=nums[i] && i + k < nums.size(); k++){
+                visit[i+k] = min(visit[i+k], visit[i]+1);
+            }
+        }
+        return visit[nums.size()-1];
+    }
+};
+
+// extra testcases:
+// [2,3,1,1,4,0,5,3,3,1,5,2,6,7,2,0,2,4,6,2]
+// [3,2,1,4,0,3,6,3,6,3,2,5,2,0,0,0,1,2,45]
+// [2,3,0,0,4]
+// [2,5,0,0]
+// [1,1,2,2,0,1,1]
+// [2,3,0,1,4]
