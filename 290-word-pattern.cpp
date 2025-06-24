75f8dbedf1911c23e906764a56568f2fa3e7fdff
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-23 20:43:48 -0400

    Create 290-word-pattern.cpp

---
+++ 290-word-pattern.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/word-pattern
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        istringstream ss(s);
+        string word;
+        unordered_map<string, char> m;
+        unordered_map<char, string> m_rev;
+        int index = 0;
+        while(ss >> word){
+            if(index == pattern.size())
+                return false;
+            
+            if(m.find(word) != m.end()){
+                if(m[word] != pattern[index])
+                    return false;
+            }
+            if(m_rev.find(pattern[index]) != m_rev.end()){
+                if(word != m_rev[pattern[index]])
+                    return false;
+            }
+            m[word] = pattern[index];
+            m_rev[pattern[index]] = word;
+
+            index++;
+        }
+        return index == pattern.size();
+    }
+};
+
+// Extra testcases:
+// "jquery"
+// "jquery"
