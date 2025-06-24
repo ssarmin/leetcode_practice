5f0edb9b5c3fa1a752a97b8cf5f0210c9189a9ff
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-24 17:57:16 -0400

    Update 162-find-peak-element.cpp

--- 162-find-peak-element.cpp
+++ 162-find-peak-element.cpp
@@ -1,4 +1,23 @@
 //https://leetcode.com/problems/find-peak-element
+// optimized code:
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+
+        while(start < end){
+            int mid = (end - start)/2 + start;
+            if(nums[mid] < nums[mid+1]){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+};
+
 
 class Solution {
 public:
