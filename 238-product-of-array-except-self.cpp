030639bdd572f023109d223a5c4cb8bfcee7a1ed
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-28 17:58:34 -0400

    Update 238-product-of-array-except-self.cpp

--- 238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self.cpp
@@ -1,4 +1,26 @@
 //https://leetcode.com/problems/product-of-array-except-self/description/
+
+// optimized code:
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res(nums.size(), 1);
+        int prefix = 1;
+        for(int i=0; i<nums.size(); i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for(int i=nums.size()-1; i>=0; i--){
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+};
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
