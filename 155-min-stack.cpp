8b5a9ae18e0c7d87601122d900add48d6037acff
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-28 14:05:30 -0400

    Update 155-min-stack.cpp

--- 155-min-stack.cpp
+++ 155-min-stack.cpp
@@ -1,5 +1,44 @@
 // https://leetcode.com/problems/min-stack
 
+
+// more concise code:
+class MinStack {
+public:
+    vector<pair<int, int>> v;//<val, min>
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(v.size() == 0){
+            v.push_back({val, val});
+        }else{
+            v.push_back({val, min(val, v[v.size()-1].second)});
+        }
+    }
+    
+    void pop() {
+        v.pop_back();
+    }
+    
+    int top() {
+        return v[v.size()-1].first;
+    }
+    
+    int getMin() {
+        return v[v.size()-1].second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+
 class MinStack {
 public:
     vector<pair<int, int>> v; //<val, min>
