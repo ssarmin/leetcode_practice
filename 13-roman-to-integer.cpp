d6b0cd27f82dc4960c49b81d45119cd95c802f0b
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-28 17:04:50 -0400

    Create 13-roman-to-integer.cpp

---
+++ 13-roman-to-integer.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/roman-to-integer
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+
+        int prev = m[s[0]];
+        int val = prev;
+        for(int i=1; i<s.size(); i++){
+            if(prev < m[s[i]]){
+                val = val - prev - prev;
+            }
+            val += m[s[i]];
+            prev = m[s[i]];
+        }
+
+        return val;
+    }
+};
+
+// "MCDLXXVI"
+// "MCMXCIV"
