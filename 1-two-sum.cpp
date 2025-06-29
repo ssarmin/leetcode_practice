9761d20183213fea7ceb723852b186afd46fe9f0
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-06-28 21:11:14 -0400

    Create 1-two-sum.cpp

---
+++ 1-two-sum.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/two-sum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m.find(nums[i]) != m.end()){
+                return {m[nums[i]], i};
+            }
+            m[target-nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+// [-1000000000,-1000000000,0,-2]
+// -2
+// [3,2,4]
+// 6
