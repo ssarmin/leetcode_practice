4080dfaac6e1e42757532d1f67ba2f017d365a0a
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-07-10 13:17:13 -0400

    Update 70-Climbing_Stairs.cpp

--- 70-Climbing_Stairs.cpp
+++ 70-Climbing_Stairs.cpp
@@ -1,5 +1,30 @@
 //https://leetcode.com/problems/climbing-stairs/description/
 
+// optimized solution:
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n < 4){
+            return n;
+        }
+
+        int val1 = 2;
+        int val2 = 3;
+        for(int i=4; i<=n; i++){
+            int val = val1+val2;
+            val1 = val2;
+            val2 = val;
+        }
+
+        return val2;
+    }
+};
+//Time complexity: O(n)
+//Space complexity: O(1)
+
+
+
+//Bottom-up / Tabulation solution
 class Solution {
 public:
     int climbStairs(int n) {
@@ -14,3 +39,38 @@ public:
         return v[n];
     }
 };
+//Time complexity: O(n)
+//Space complexity: O(n)
+
+
+
+//recursive solution
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n < 4)
+            return n;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+};
+//Time complexity: O(2^n)
+//Space complexity: O(n) stack
+
+//Top down with memorization solution
+class Solution {
+public:
+    unordered_map<int, int> m;
+    int climbStairs(int n) {
+        if(m.find(n) != m.end()){
+            return m[n];
+        }
+        if(n < 4){
+            m[n] = n;
+            return n;
+        }
+        m[n] = climbStairs(n-1) + climbStairs(n-2);
+        return m[n];
+    }
+};
+// Time complexity: O(n)
+// Space Complexity: O(n)
