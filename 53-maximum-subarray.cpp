f7a75379eb8d6d7f752a3662419770d0bc53224c
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-07-18 19:44:57 -0400

    Update 53-maximum-subarray.cpp

--- 53-maximum-subarray.cpp
+++ 53-maximum-subarray.cpp
@@ -1,4 +1,16 @@
 //https://leetcode.com/problems/maximum-subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxVal = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            nums[i] = max(nums[i], nums[i]+nums[i-1]);
+            maxVal = max(maxVal, nums[i]);
+        }
+
+        return maxVal;
+    }
+};
 //optimized
 //bottom-up solution
 class Solution {
