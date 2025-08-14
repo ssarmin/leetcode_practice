902f6e2a43e30fcc5f918ed4d43ec598f627b6c6
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-08-14 18:17:19 -0400

    Update 994-rotting-oranges.cpp

--- 994-rotting-oranges.cpp
+++ 994-rotting-oranges.cpp
@@ -2,32 +2,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
+        int good = 0;
+        queue<pair<int, int>> q;
         int row = grid.size();
         int col = grid[0].size();
 
-        queue<pair<int, int>> q;
         for(int r=0; r<row; r++){
             for(int c=0; c<col; c++){
-                if(grid[r][c] == 2){
+                if(grid[r][c] == 1)
+                    good++;
+                else if(grid[r][c] == 2)
                     q.push({r, c});
-                }
             }
         }
+
+        if(good == 0)
+            return 0;
+        if(q.size() == 0)
+            return -1;
+
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
         int count = 0;
         while(!q.empty()){
             int size = q.size();
             bool flag = false;
-            for(int s=0; s<size; s++){
-                int r = q.front().first;
-                int c = q.front().second;
+            while(size--){
+                auto [r, c] = q.front();
                 q.pop();
-
                 for(int i=0; i<4; i++){
-                    int x = r + dx[i];
-                    int y = c + dy[i];
-                    if(x >= 0 && y >= 0 && x < row && y < col && grid[x][y] == 1){
+                    int x = dx[i] + r;
+                    int y = dy[i] + c;
+
+                    if(x >= 0 && x < row && y >= 0 && y < col && grid[x][y] == 1){
+                        good--;
                         grid[x][y] = 2;
                         q.push({x, y});
                         flag = true;
@@ -39,15 +47,7 @@ public:
             }
         }
 
-        for(int r=0; r<row; r++){
-            for(int c=0; c<col; c++){
-                if(grid[r][c] == 1){
-                    return -1;
-                }
-            }
-        }
-
-        return count;
+        return good == 0? count: -1;
     }
 };
 // [[1,2,1,1,2,1,1]]
