439ba8175964dd23f4b08e3f1817ea47c4c55fa7
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-09-17 13:29:30 -0400

    Create 933-number-of-recent-calls.cpp

---
+++ 933-number-of-recent-calls.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/number-of-recent-calls
+class RecentCounter {
+public:
+    vector<pair<int, int>> v;
+    int count;
+    RecentCounter() {
+        count = 0;
+    }
+    
+    int ping(int t) {
+        v.push_back({t, count});
+        int res = 0;
+        for(int i=v.size()-1; i>=0; i--){
+            if(v[i].first < (t-3000))
+                break;
+            res++;
+        }
+        count++;
+        return res;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
