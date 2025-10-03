af0e88fef554282d370779ebd39e5d6a95d43478
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-10-03 11:28:13 -0400

    Update 74-search-a-2d-matrix.cpp

--- 74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix.cpp
@@ -1,4 +1,33 @@
-// https://leetcode.com/problems/search-a-2d-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+// https://leetcode.com/problems/search-a-2d-matrix
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int start = 0;
+        int end = row*col - 1;
+        while(start <= end){
+            int mid = (end - start)/2 + start;
+            int r = mid/col;
+            int c = mid%col;
+            if(matrix[r][c] == target)
+                return true;
+            if(matrix[r][c] > target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+};
+// Time complexity: O(log(m*n))
+// This approach won't work if it's not sorted globally (only each row is sorted, but, no relation with the previous row's last element)
+
+
+
 // Recursive solution:
 
 class Solution {
@@ -53,3 +82,30 @@ public:
         return false;
     }
 };
+// Time complexity: O(M+N)
+
+
+// [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+// 3
+// [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+// 13
+// [[1],[3],[5]]
+// 0
+// [[1],[3],[5]]
+// 1
+// [[1],[3],[5]]
+// 5
+// [[1, 3]]
+// 3
+// [[1, 3]]
+// 2
+// [[1]]
+// 1
+// [[1],[3],[5]]
+// 3
+// [[1], [3], [5], [7], [9], [11], [13], [15], [17], [19], [21], [23], [25], [27], [29], [31], [33], [35], [37], [39], [41], [43], [45], [47], [49], [51], [53], [55], [57], [59], [61], [63], [65], [67], [69], [71], [73], [75], [77], [79], [81], [83], [85], [87], [89], [91], [93], [95], [97], [99]]
+// 3
+// [[1],[3],[5],[6]]
+// 3
+// [[1, 3]]
+// 2
