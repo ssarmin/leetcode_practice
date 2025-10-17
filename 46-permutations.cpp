81ffac8843e3b94ac763285c017a609e0fac66ea
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-10-17 10:09:19 -0400

    Update 46-permutations.cpp

--- 46-permutations.cpp
+++ 46-permutations.cpp
@@ -1,6 +1,26 @@
 //https://leetcode.com/problems/permutations
 class Solution {
 public:
+    void helper(vector<int> &nums, vector<vector<int>> &res, int index){
+        if(index >= nums.size()){
+            res.push_back(nums);
+        }else{
+            for(int i=index; i<nums.size(); i++){
+                swap(nums[i], nums[index]);
+                helper(nums, res, index+1);
+                swap(nums[i], nums[index]);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        helper(nums, res, 0);
+        return res;
+    }
+};
+
+class Solution {
+public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         stack<tuple<vector<int>, int, int>> s;
