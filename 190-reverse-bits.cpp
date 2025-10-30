adb572e1a1406d24fd4c12d0f860201d686d5f4d
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-10-30 10:07:36 -0400

    Update 190-reverse-bits.cpp

--- 190-reverse-bits.cpp
+++ 190-reverse-bits.cpp
@@ -1,6 +1,21 @@
 // https://leetcode.com/problems/reverse-bits
 class Solution {
 public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i=31; i>=0; i--){
+            int bit = (n >> i) & 1;
+            int j = 31 - i;
+            res = res | (bit << j);
+        }
+        return res;
+    }
+};
+//2147483646
+//0
+
+class Solution {
+public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t res = 0;
         for(int i=0; i<32; i++){
