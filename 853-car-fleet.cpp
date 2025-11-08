456a7f217f67ca6566a6d9c25792fb3267214aa5
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-11-07 21:24:01 -0500

    Update 853-car-fleet.cpp

--- 853-car-fleet.cpp
+++ 853-car-fleet.cpp
@@ -1,13 +1,33 @@
 //https://leetcode.com/problems/car-fleet
-10
-[0,4,2]
-[2,1,3]
-31
-[5,26,18,25,29,21,22,12,19,6]
-[7,6,6,4,3,4,9,7,6,4]
-20
-[6,2,17]
-[3,9,2]
-10
-[8,3,7,4,6,5]
-[4,4,4,4,4,4]
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> v;
+        for(int i=0; i<position.size(); i++){
+            v.push_back({position[i], speed[i]});
+        }
+        sort(v.rbegin(), v.rend());
+        stack<double> s;
+        int res = 0;
+        for(int i=0; i<v.size(); i++){
+            double t = (double)(target - v[i].first)/(1.0*v[i].second);
+            if(s.empty() || s.top() < t){
+                s.push(t);
+            }
+        }
+        return s.size();
+    }
+};
+
+// 10
+// [0,4,2]
+// [2,1,3]
+// 31
+// [5,26,18,25,29,21,22,12,19,6]
+// [7,6,6,4,3,4,9,7,6,4]
+// 20
+// [6,2,17]
+// [3,9,2]
+// 10
+// [8,3,7,4,6,5]
+// [4,4,4,4,4,4]
