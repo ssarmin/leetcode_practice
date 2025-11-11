668ecd017419c984e53ed3959ce257923d88e228
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-11-11 09:06:20 -0500

    Update 1202-smallest-string-with-swaps.cpp

--- 1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps.cpp
@@ -1,4 +1,51 @@
 //https://leetcode.com/problems/smallest-string-with-swaps
+class Solution {
+public:
+    vector<int> dfs(int i, unordered_set<int> &visit, unordered_map<int, unordered_set<int>> &m){
+        queue<int> q;
+        q.push(i);
+        vector<int> res;
+        while(!q.empty()){
+            i = q.front();
+            q.pop();
+            if(visit.count(i))continue;
+            res.push_back(i);
+            visit.insert(i);
+            for(auto a: m[i]){
+                if(!visit.count(a)){
+                    q.push(a);
+                }
+            }
+        }
+        return res;
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        unordered_map<int, unordered_set<int>> m;
+        for(auto p: pairs){
+            m[p[0]].insert(p[1]);
+            m[p[1]].insert(p[0]);
+        }
+        unordered_set<int> visit;
+        for(int i=0; i<s.size(); i++){
+            if(!visit.count(i)){
+                vector<int> res = dfs(i, visit, m);
+                vector<char> v_temp;
+                for(auto index: res){
+                    v_temp.push_back(s[index]);
+                }
+                sort(res.begin(), res.end());
+                sort(v_temp.begin(), v_temp.end());
+                int k=0;
+                for(auto index: res){
+                    s[index] = v_temp[k];
+                    k++;
+                }
+            }
+        }
+        return s;
+    }
+};
+
 "udyyek"
 [[3,3],[3,0],[5,1],[3,1],[3,4],[3,5]]
 "qdwyt"
