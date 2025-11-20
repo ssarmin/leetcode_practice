5398675db5bd373180c6478f45f3b27337993fea
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-11-20 16:16:01 -0500

    Update 45-jump-game-ii.cpp

--- 45-jump-game-ii.cpp
+++ 45-jump-game-ii.cpp
@@ -14,6 +14,23 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int max_index = 0;
+        int jump = 0;
+        int end_index = 0;
+        for(int i=0; i<nums.size()-1; i++){
+            max_index = max(max_index, nums[i]+i);
+            if(i == end_index){
+                jump++;
+                end_index = max_index;
+            }
+        }
+        return jump;
+    }
+};
 // extra testcases:
 /*
 [2,3,1,1,4,0,5,3,3,1,5,2,6,7,2,0,2,4,6,2]
