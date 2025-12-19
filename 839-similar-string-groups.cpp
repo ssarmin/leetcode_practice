01f20a2df6271b14c1e9ce9ce50ebdff25d96b25
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-12-19 09:10:08 -0500

    Update 839-similar-string-groups.cpp

--- 839-similar-string-groups.cpp
+++ 839-similar-string-groups.cpp
@@ -1,4 +1,62 @@
 //https://leetcode.com/problems/similar-string-groups
+class Solution {
+public:
+    unordered_map<string, string> parent;
+    string find(string node){
+        string p = parent[node];
+        while(p != node){
+            node = p;
+            p = parent[node];
+        }
+        return p;
+    }
+    void unite(string node1, string node2){
+        string p1 = find(node1);
+        string p2 = find(node2);
+        if(p1 == p2)
+            return;
+        parent[p1] = p2;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        unordered_set<string> s;
+        sort(strs.begin(), strs.end());
+        for(int i=0; i<strs.size(); i++){
+            parent[strs[i]] = strs[i];
+        }
+        for(int i=0; i<strs.size(); i++){
+            for(int j=i+1; j<strs.size(); j++){
+                if(strs[i] == strs[j]){
+                    unite(strs[i], strs[j]);
+                }else{
+                    int diff = 0;
+                    pair<int, int> p = {-1, -1};
+                    for(int l=0; l<strs[i].size() && diff < 3; l++){
+                        if(strs[i][l] != strs[j][l]){
+                            diff++;
+                            if(p.first == -1){
+                                p.first = l;
+                            }else{
+                                p.second = l;
+                            }
+                        }
+                    }
+                    if(diff < 3){
+                        string temp = strs[i];
+                        swap(temp[p.first], temp[p.second]);
+                        if(temp == strs[j]){
+                            unite(strs[i], strs[j]);
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<strs.size(); i++){
+            s.insert(find(strs[i]));
+        }
+        return s.size();
+    }
+};
+/*
 ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaebdfc"]
 ["kccomwcgcs", "socgcmcwkc", "sgckwcmcoc", "coswcmcgkc", "cowkccmsgc", "cosgmccwkc", "sgmkwcccoc", "coswmccgkc", "kowcccmsgc", "kgcomwcccs"]
 ["koqnn","knnqo","noqnk","nqkon"]
@@ -11,3 +69,4 @@
 ["xxyy","xyxy","yxxy","yyxx","yxyx"]
 ["abcdefghijkl","abcedfghijkl","abcdeghfijkl","bacdefghijkl","abcdefghijkl"]
 ["mnoopp","monopp","mnoopp","mnoopp"]
+*/
