563a4b7076992a548d5d6de1bbaf98ef1fb0c832
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2025-12-23 15:50:06 -0500

    Update 340-longest-substring-with-at-most-k-distinct-characters.cpp

--- 340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,25 +1,60 @@
 //https://www.geeksforgeeks.org/dsa/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
 //https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters
 class Solution {
-  public:
-    int longestKSubstr(string &s, int k) {
-        // code here
-        unordered_map<char, int> m;
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
         int left = 0;
-        int res = INT_MIN;
-        for(int right = 0; right < s.size(); right++){
-            m[s[right]]++;
-            while(left < right && m.size() > k){
+        unordered_map<char, int> m;
+        int max_len = 0;
+        for(int i=0; i<s.size(); i++){
+            m[s[i]]++;
+            while(m.size() > k){
                 m[s[left]]--;
                 if(m[s[left]] == 0){
                     m.erase(s[left]);
                 }
                 left++;
             }
-            if(m.size() == k){
-                res = max(res, right - left + 1);
-            }
+            max_len = max(max_len, i - left + 1);
         }
-        return res == INT_MIN? -1: res;
+        return max_len;
     }
 };
+/*
+"eceba"
+2
+"aa"
+1
+"a"
+0
+"abc"
+1
+"abc"
+2
+"abc"
+3
+"aaaaabbbbb"
+1
+"aaaaabbbbb"
+2
+"abababab"
+2
+"abababab"
+1
+"abcabcabc"
+2
+"abcabcabc"
+3
+"aaabbbccc"
+2
+"aaabbbccc"
+1
+"abcdefghijklmnopqrstuvwxyz"
+5
+"abcdefghijklmnopqrstuvwxyz"
+26
+"aaaaaaaaaaaaaaaaaaaa"
+50
+"abaccc"
+2
+*/
