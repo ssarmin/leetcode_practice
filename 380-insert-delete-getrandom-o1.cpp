97f9f2a852b45cc3e2d9cf1f403ccc64d7dd99cd
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2026-02-22 06:52:56 -0500

    Update 380-insert-delete-getrandom-o1.cpp

--- 380-insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1.cpp
@@ -34,20 +34,21 @@ public:
  * int param_3 = obj->getRandom();
  */
 
-
-// ["RandomizedSet","insert","insert","remove","insert","insert","insert","remove","remove","insert","remove","insert","insert","insert","insert","insert","getRandom","insert","remove","insert","insert"]
-// [[],[3],[-2],[2],[1],[-3],[-2],[-2],[3],[-1],[-3],[1],[-2],[-2],[-2],[1],[],[-2],[0],[-3],[1]]
-// ["RandomizedSet","remove","remove","insert","getRandom","remove","insert"]
-// [[],[0],[0],[0],[],[0],[0]]
-// ["RandomizedSet","insert","insert","insert","getRandom","insert","remove","getRandom","insert","getRandom","insert"]
-// [[],[0],[0],[2],[],[3],[1],[],[3],[],[2]]
-// ["RandomizedSet","remove","remove","insert","getRandom","remove","insert"]
-// [[],[0],[0],[0],[],[0],[0]]
-// ["RandomizedSet", "insert", "insert", "remove", "remove"]
-// [[], [5], [5], [5], [5]]
-// ["RandomizedSet", "insert", "insert", "insert", "remove", "getRandom"]
-// [[], [10], [20], [30], [20], []]
-// ["RandomizedSet", "insert", "remove", "insert", "getRandom"]
-// [[], [2], [2], [2], []]
-// ["RandomizedSet", "insert", "insert", "getRandom"]
-// [[], [1], [1], []]
+/*
+["RandomizedSet","insert","insert","remove","insert","insert","insert","remove","remove","insert","remove","insert","insert","insert","insert","insert","getRandom","insert","remove","insert","insert"]
+[[],[3],[-2],[2],[1],[-3],[-2],[-2],[3],[-1],[-3],[1],[-2],[-2],[-2],[1],[],[-2],[0],[-3],[1]]
+["RandomizedSet","remove","remove","insert","getRandom","remove","insert"]
+[[],[0],[0],[0],[],[0],[0]]
+["RandomizedSet","insert","insert","insert","getRandom","insert","remove","getRandom","insert","getRandom","insert"]
+[[],[0],[0],[2],[],[3],[1],[],[3],[],[2]]
+["RandomizedSet","remove","remove","insert","getRandom","remove","insert"]
+[[],[0],[0],[0],[],[0],[0]]
+["RandomizedSet", "insert", "insert", "remove", "remove"]
+[[], [5], [5], [5], [5]]
+["RandomizedSet", "insert", "insert", "insert", "remove", "getRandom"]
+[[], [10], [20], [30], [20], []]
+["RandomizedSet", "insert", "remove", "insert", "getRandom"]
+[[], [2], [2], [2], []]
+["RandomizedSet", "insert", "insert", "getRandom"]
+[[], [1], [1], []]
+*/
