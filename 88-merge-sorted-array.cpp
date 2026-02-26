349dce4ce602d07d4f31191b49f5ae3af20f52c0
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2026-02-26 09:52:14 -0500

    Update 88-merge-sorted-array.cpp

--- 88-merge-sorted-array.cpp
+++ 88-merge-sorted-array.cpp
@@ -3,37 +3,33 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n == 0)
-            return;
-
-        int index1 = 0;
-        int index2 = 0;
-        for(; index1<m; index1++){
-            if(nums1[index1] > nums2[index2]){
-                swap(nums1[index1], nums2[index2]);
-                int i = index2;
-                while(i+1 < n && nums2[i] > nums2[i+1]){
-                    swap(nums2[i], nums2[i+1]);
-                    i++;
-                }
+        int index1 = m-1;
+        int index2 = n-1;
+        int i = m+n-1;
+        for(; i>=0 && index1 >= 0 && index2 >= 0; i--){
+            if(nums1[index1] >= nums2[index2]){
+                nums1[i] = nums1[index1];
+                index1--;
+            }else{
+                nums1[i] = nums2[index2];
+                index2--;
             }
         }
-        for(; index1<m+n; index1++){
-            nums1[index1] = nums2[index2];
-            index2++;
+        for(; i>=0 && index2 >= 0; i--){
+            nums1[i] = nums2[index2];
+                index2--;
         }
     }
 };
 
-Time Complexity: O(Mn)
+/* Extra testcases:
 
-// Extra testcases:
-// [2,3,5,7,0,0,0,0]
-// 4
-// [2,4,5,6]
-// 4
-
-// [2,5,6,0,0,0]
-// 3
-// [1,2,3]
-// 3
+[2,3,5,7,0,0,0,0]
+4
+[2,4,5,6]
+4
+[2,5,6,0,0,0]
+3
+[1,2,3]
+3
+*/
