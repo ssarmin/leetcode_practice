f5a3947134d042cde6fe0a6d8c189aca33e79cae
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2026-06-26 09:51:26 -0400

    Implement merge intervals solution in Python

---
+++ 56-merge-intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        intervals.sort(key = lambda x: x[0])
+        res = [intervals[0]]
+        for interval in intervals[1:]:
+            if res[-1][1] >= interval[0]:
+                res[-1][1] = max(interval[1], res[-1][1])
+            else:
+                res.append(interval)
+        return res
+
+        
