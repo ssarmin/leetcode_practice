29eaf252281be61892d709c3cff0b7bfa71ee9bf
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2026-07-20 16:57:03 -0400

    Clean up comments in 49-group-anagrams.cpp
    Removed commented-out test cases and added a block comment for clarity.

--- 49-group-anagrams.cpp
+++ 49-group-anagrams.cpp
@@ -15,5 +15,7 @@ public:
         return res;
     }
 };
-// ["abbbbbbbbbbb", "baaaaaaaaaaa"]
-// ["hos","boo","nay","deb","wow","bop","bob","brr","hey","rye","eve","elf","pup","bum","iva","lyx","yap","ugh","hem","rod","aha","nam","gap","yea","doc","pen","job","dis","max","oho","jed","lye","ram","pup","qua","ugh","mir","nap","deb","hog","let","gym","bye","lon","aft","eel","sol","jab"]
+/*
+["abbbbbbbbbbb", "baaaaaaaaaaa"]
+["hos","boo","nay","deb","wow","bop","bob","brr","hey","rye","eve","elf","pup","bum","iva","lyx","yap","ugh","hem","rod","aha","nam","gap","yea","doc","pen","job","dis","max","oho","jed","lye","ram","pup","qua","ugh","mir","nap","deb","hog","let","gym","bye","lon","aft","eel","sol","jab"]
+*/
