08dbcb099c23ee3a153175d741c3f2cfeb5fd5de
Saiyma Sarmin <46971821+ssarmin@users.noreply.github.com>
2026-07-23 09:29:48 -0400

    Add cycle detection method in linked list
    Implement cycle detection in a linked list using Floyd's Tortoise and Hare algorithm.

---
+++ 142-linked-list-cycle-ii.cpp
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/linked-list-cycle-ii
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
